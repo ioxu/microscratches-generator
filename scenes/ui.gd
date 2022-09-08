9eb0ffa37b167a35aea29d7157a45ad68f045399
ioxu <ben@ioxu.com>
2022-09-08 08:01:45 +0100

    resolution label

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,11 +1,19 @@
 extends Node
 
-var zoom_label : Label
+export(NodePath) onready var viewport = get_node( viewport )
 
+var zoom_label : Label
+var resolution_label : Label
 
 func _ready() -> void:
 	zoom_label = self.find_node("zoom_label")
+	resolution_label = self.find_node("resolution_label")
+	resolution_label.text = "resolution : %s"%viewport.get_size()
 
 
 func _on_display_zoom_changed(new_zoom) -> void:
 	zoom_label.text = "zoom : %0.2f"%new_zoom
+
+
+func _on_Viewport_size_changed() -> void:
+	resolution_label.text = "resolution : %s"%viewport.get_size()
